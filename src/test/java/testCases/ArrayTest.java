1012bcdfef001eb05b604388da1e9b90f73c2243
ayana.kondiyara@gmail.com <ayana.kondiyara@gmail.com>
2026-04-22 22:22:39 -0400

    Initial framework set up by Theresa, Syeda, & Ayana

---
+++ src/test/java/testCases/ArrayTest.java
@@ -0,0 +1,17 @@
+package testCases;
+
+import base.BaseClass;
+import org.testng.annotations.Test;
+import pageObjects.ArrayPage;
+
+public class ArrayTest extends BaseClass {
+
+    @Test
+    public void testGraphCreation() {
+        // using the driver inherited from BaseClass
+        ArrayPage graphPage = new ArrayPage(getDriver());
+
+        // eg
+        graphPage.clickOnGraphLink();
+    }
+}
